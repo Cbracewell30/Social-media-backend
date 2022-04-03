2c03eaf03cd926f08cfb49ec88b39c62f377d17f
Chris <chrisbracewell35@gmail.com>
2022-04-03 11:51:58 -0400

    updating database

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const UserRoutes = require("./User-routes");
-const commentRoutes = require("./Thoughts-routes");
+const ThoughtsRoutes = require("./Thoughts-routes");
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
-router.use("/users", pizzaRoutes);
-router.use("/thoughts", commentRoutes);
+router.use("/users", UserRoutes);
+router.use("/thoughts", ThoughtsRoutes);
 
 module.exports = router;
