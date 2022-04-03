a01e9d11d6874950cd18b90298f0e498f0d44eae
Chris <chrisbracewell35@gmail.com>
2022-04-03 12:45:06 -0400

    finishing user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -71,6 +71,42 @@ const UserController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+  // adding a friend
+
+  addFriend({ params, body }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $addToSet: { firends: params.friendId } },
+      {
+        new: true,
+      }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $pull: { firends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = UserController;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const UserRoutes = require("./User-routes");
 const ThoughtsRoutes = require("./Thoughts-routes");
-// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+
+// Creating routes for USer and thoughts
 router.use("/users", UserRoutes);
 router.use("/thoughts", ThoughtsRoutes);
 
